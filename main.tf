1b4e9f8f172a74bb917bc3e96aa602764a15f1ab
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-03-20 02:27:18 +0000

    Connect via Systems Manager

--- main.tf
+++ main.tf
@@ -14,10 +14,32 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0eba6c58b7918d3a1"
-  instance_type = "t2.micro"
+  ami                  = "ami-0eba6c58b7918d3a1"
+  instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.profile.name
 
   tags = {
     Name = var.instance_name
   }
+}
+
+# Instance Profile
+resource "aws_iam_instance_profile" "profile" {
+  role = aws_iam_role.role.name
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "role" {
+  assume_role_policy  = data.aws_iam_policy_document.assume_role.json
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
 }
